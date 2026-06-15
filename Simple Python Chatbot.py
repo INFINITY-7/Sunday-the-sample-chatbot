05d5ee496380e36f722b9247da46223e5e3ea8ee
INFINITY-7 <110834032+INFINITY-7@users.noreply.github.com>
2026-06-15 10:50:41 +0530

    Add files via upload

---
+++ Simple Python Chatbot.py
@@ -0,0 +1,22 @@
+# Simple Python Chatbot
+
+print("ChatBot: Hello! Type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ").lower()
+
+    if user_input == "hello":
+        print("ChatBot: Hi there!")
+    
+    elif user_input == "how are you":
+        print("ChatBot: I'm fine, thanks!")
+    
+    elif user_input == "what is your name":
+        print("ChatBot: I'm Sunday .")
+    
+    elif user_input == "bye":
+        print("ChatBot: Goodbye!")
+        break
+    
+    else:
+        print("ChatBot: Sorry, I don't understand.") Give me github read me file
